1ee5cc2d3a22f8ffaebc196a311ab82b40d6910f
JunbugSpark <parkseojun118@gmail.com>
2024-11-12 19:52:32 -0500

    Added another button

--- DigElecWeek10.ino
+++ DigElecWeek10.ino
@@ -1,14 +1,16 @@
 #include "BetterButton.h"
 
-int button1Pin = 36;
-int button2Pin = 35;
-int button3Pin = 34;
-int button4Pin = 33;
+int button1Pin = 37;
+int button2Pin = 36;
+int button3Pin = 35;
+int button4Pin = 34;
+int button5Pin = 33;
 
 BetterButton button1(button1Pin, 0);
 BetterButton button2(button2Pin, 1);
 BetterButton button3(button3Pin, 2);
 BetterButton button4(button4Pin, 3);
+BetterButton button5(button5Pin, 4);
 
 void setup() {
   Serial.begin(9600);
@@ -21,6 +23,8 @@ void setup() {
   button3.releaseHandler(onRelease);
   button4.pressHandler(onPress);
   button4.releaseHandler(onRelease);
+  button5.pressHandler(onPress);
+  button5.releaseHandler(onRelease);
 }
 
 void loop() {
@@ -28,12 +32,13 @@ void loop() {
   button2.process();
   button3.process();
   button4.process();
+  button4.process();
 }
 
 void onPress(int val) {
   Serial.print(val);
   Serial.println(" on");
-  usbMIDI.sendNoteOn(val + 60, 127, 1);
+  usbMIDI.sendNoteOn(val + 60, 90, 1);
 }
 
 void onRelease(int val) {
